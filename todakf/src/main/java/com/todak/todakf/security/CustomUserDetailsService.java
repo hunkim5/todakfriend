8c8490fd01ee605ba217a66bb32049715b882cb6
hunkim5 <hunkim5@naver.com>
2025-06-05 00:26:49 +0900

    aaa

--- todakf/src/main/java/com/todak/todakf/security/CustomUserDetailsService.java
+++ todakf/src/main/java/com/todak/todakf/security/CustomUserDetailsService.java
@@ -6,22 +6,21 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.todak.todakf.user.dto.UserDto;
-import com.todak.todakf.user.mapper.UserMapper;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService{
 
-	private final UserMapper userMapper;
+//	private final UserMapper userMapper;
 
-    public CustomUserDetailsService(UserMapper userMapper) {
-        this.userMapper = userMapper;
-    }
+//    public CustomUserDetailsService(UserMapper userMapper) {
+//        //this.userMapper = userMapper;
+//    }
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserDto user = userMapper.findByUsername(username);
-        if (user == null) {
-            throw new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + username);
-        }
-        return new CustomUserDetails(user);
+        //UserDto user = userMapper.findByUsername(username);
+//        if (user == null) {
+//            throw new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + username);
+//        }
+        return new CustomUserDetails(new UserDto());
     }
 }
