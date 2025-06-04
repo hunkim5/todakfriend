3167c7fa5d342179ade8c9b426e1747f44730e29
hunkim5 <hunkim5@naver.com>
2025-06-04 21:57:38 +0900

    aaa

---
+++ todakf/src/main/java/com/todak/todakf/TodakfApplication.java
@@ -0,0 +1,13 @@
+package com.todak.todakf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodakfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodakfApplication.class, args);
+	}
+
+}
---
+++ todakf/src/test/java/com/todak/todakf/TodakfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todak.todakf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodakfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
